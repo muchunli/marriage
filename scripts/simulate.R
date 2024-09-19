e6f3b1d4b4c3582616ebd509fb272bc71e8ebae6
muchunli <linicole102021@gmail.com>
2024-09-19 17:51:30 -0400

    Update and rename 00-simulate_data.R to simulate.R
    simulation

---
+++ scripts/simulate.R
@@ -0,0 +1,42 @@
+#### Preamble ####
+# Purpose: Simulates... [...UPDATE THIS...]
+# Author: Rohan Alexander [...UPDATE THIS...]
+# Date: 11 February 2023 [...UPDATE THIS...]
+# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(dplyr)
+
+# [...UPDATE THIS...]
+
+#### Simulate data ####
+# [...ADD CODE HERE...]
+set.seed(304)
+
+start_date <- as.Date("2018-01-01")
+end_date <- as.Date("2023-12-31")
+
+number_of_dates <- 100
+
+data <- 
+  tibble(
+    dates = as.Date(
+      runif(
+        n = number_of_dates,
+        min = as.numeric(start_date),
+        max = as.numeric(end_date)
+      ),
+      origin = "1970-01-01"
+    ),
+      number_of_marriage = rpois(n = number_of_dates, lambda = 15)
+  )
+
+
+###Write_csv
+write_csv(data, file = "data/raw_data/simulated.csv")
+
